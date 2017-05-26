c599f6c03c08768062fd30cf6e994a532f72defb
dahu yao <dahu yao>
2017-05-26 16:23:22 +0800

    完成新闻早知道模块即爬取宝钢首钢新闻

--- js/basepath.js
+++ js/basepath.js
@@ -1,3 +1,5 @@
 //var basepath = "http://121.42.186.54:8081/"
-var basepath = "http://47.92.89.156:8503/"
-//var basepath = "http://192.168.191.1:8503/"
+
+
+//var basepath = "http://47.92.89.156:8503/"
+var basepath = "http://192.168.191.1:8503/"
